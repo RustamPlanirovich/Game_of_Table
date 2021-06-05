6a7bdb692586614b8203f65ffe2f2a1d068d8bad
rustam <israr.comsas@gmail.com>
2021-06-05 17:07:44 +0700

    - Добавил все нужные зависимости - Создал репозиторий в firebase

--- app/src/main/java/com/example/gameoftable/MainActivity.kt
+++ app/src/main/java/com/example/gameoftable/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gameoftable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var name = MutableLiveData<String>()
+        name.value = "Hello"
+
     }
 }
